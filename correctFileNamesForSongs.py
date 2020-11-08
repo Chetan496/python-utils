56215dd134b737f1743e8b16666f9772aed9a252
Chetan Yewale <yewalechetan@gmail.com>
2020-11-08 19:12:09 +0530

    completed code to correct filenames in bulk

--- correctFileNamesForSongs.py
+++ correctFileNamesForSongs.py
@@ -1,27 +1,52 @@
 import sys
 import re
-from os import listdir, mkdir
-from os.path import isfile, join, isdir, exists
+from os import listdir, mkdir, rename
+from os.path import isfile, join, isdir, exists, basename, dirname, splitext
+from pathlib import Path
+import string
 
 def main():
     if len(sys.argv) < 2:
         raise ValueError('please pass the directory containing all songs')
 
     songDir = sys.argv[1]
-    songFiles = [ dirEntry for dirEntry in listdir(songDir) if isfile(join(songDir, dirEntry)) ]
-    for song in songFiles:
-        removeNumberPrefix(song)
-    
+    songFiles = recursiveDirTreeWalker(songDir)
+    songModifications = []
+    for songFile in songFiles:
+        songName = basename(songFile)
+        songDir = dirname(songFile)
+        changeTuple = removeNumberPrefix(songName)
+        if changeTuple[0] != changeTuple[1]:
+            renameTuple = ( join(songDir, changeTuple[0]) , join(songDir, changeTuple[1] ) )
+            songModifications.append(renameTuple)
+
+    for modification in songModifications:
+        print(modification[0], "  -->  ", modification[1])
+        rename(modification[0], modification[1])
+
+
+
     
 def removeNumberPrefix(songName):
-    pattern = r'^\d+'
+    pattern = r'^\d+(\.|-|\s|_)*'
     modifiedName = re.sub(pattern, '', songName)
-    print("rename %s -> %s "% (songName ,modifiedName))
-
-
+    if splitext(modifiedName)[0].isupper():
+        modifiedName = string.capwords(modifiedName)
+    return (songName, modifiedName)
 
 def recursiveDirTreeWalker(dirPath):
-    files = [ dirEntry for dirEntry in listdir(dirPath) if isfile(join(dirPath, dirEntry)) ]
+    dirContents = listdir(dirPath)
+    files = [ join(dirPath, dirEntry) for dirEntry in dirContents if isfile(join(dirPath, dirEntry)) and isSongSuffix(join(dirPath, dirEntry)) ]
+    directories = [ dirEntry for dirEntry in dirContents if  isdir(join(dirPath, dirEntry)) ]
+    for a_dir in directories:
+        subDir = join(dirPath, a_dir )
+        files.extend(recursiveDirTreeWalker(subDir))
+    return files
+
+
+def isSongSuffix(filePath):
+    suffix = Path(filePath).suffix
+    return suffix == ".mp3" or suffix == ".MP3" or suffix == ".aac" or suffix == ".wav" or suffix == ".ogg" or suffix == ".flac"
 
 
 if __name__ == "__main__":
